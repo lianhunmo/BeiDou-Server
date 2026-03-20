34e34cb99a8e2f1f3626671c6a6eafd32bfb0b6e
lianhunmo <393811421@qq.com>
2026-03-21 07:36:45 +0800

    优化一键出售脚本

--- gms-server/scripts-zh-CN/BeiDouSpecial/一键出售.js
+++ gms-server/scripts-zh-CN/BeiDouSpecial/一键出售.js
@@ -1,8 +1,8 @@
-var status;
-var text;
-var column = ["装备", "消耗", "设置", "其他", "商城"];
-var sel;
-
+let status;
+let text;
+let column = ["装备", "消耗", "设置", "其他", "商城"];
+let inventoryType;
+let chooseType;
 
 function start() {
     levelStart();
@@ -13,7 +13,7 @@ function levelStart() {
     text = "#e出售道具#n\r\n#r注：前3页（72格）的道具都会被卖出，请注意将不出售的物品放在第4页#k\r\n\r\n";
     for (let i = 1; i <= 5; i++) {
         if (i === 1 || i === 2 || i === 4) {
-            text += "#L" + i + "#出售" + column[i - 1] + "栏的道具#l\r\n";
+            text += "#L" + i + "##b出售" + column[i - 1] + "栏的道具#k#l\r\n\r\n";
         }
     }
     // 选择出售哪一栏
@@ -22,21 +22,42 @@ function levelStart() {
 
 // 选择了背包栏
 function levelChooseInventory(choose) {
-    sel = choose;
-    const ShopFactory = Java.type('org.gms.server.ShopFactory');
     const InventoryType = Java.type('org.gms.client.inventory.InventoryType');
-    var type = InventoryType.EQUIP;
-    if (sel == 2) {
-        type = InventoryType.USE;
-    } else if (sel == 4) {
-        type = InventoryType.ETC
+    chooseType = choose;
+    if (chooseType == 2) {
+        inventoryType = InventoryType.USE;
+    } else if (chooseType == 4) {
+        inventoryType = InventoryType.ETC
+    } else if (chooseType == 1) {
+        inventoryType = InventoryType.EQUIP
+    } else {
+        cm.sendOk("不支持这种背包类型。。。");
+        cm.dispose();
+        return;
     }
+    let text = "请确认是否售出：\r\n";
     for (let i = 0; i <= 72; i++) {
-        let item = cm.getInventory(sel).getItem(i);
+        let item = cm.getInventory(chooseType).getItem(i);
         if (item) {
-            ShopFactory.getInstance().getShop(11000).sell(cm.getClient(), type, i, item.getQuantity());
+            text += "#b#z" + item.getItemId() + "##k#i" + item.getItemId() + "#\t";
         }
     }
-    cm.sendOk("出售" + column[sel - 1] + "成功！");
+    cm.sendYesNoLevel("Dispose", "SellInventory", text);
+
+}
+
+function levelSellInventory() {
+    const ShopFactory = Java.type('org.gms.server.ShopFactory');
+    for (let i = 0; i <= 72; i++) {
+        let item = cm.getInventory(chooseType).getItem(i);
+        if (item) {
+            ShopFactory.getInstance().getShop(11000).sell(cm.getClient(), inventoryType, i, item.getQuantity());
+        }
+    }
+    cm.sendOk("出售" + column[chooseType - 1] + "成功！");
+    cm.dispose();
+}
+
+function levelDispose() {
     cm.dispose();
 }
