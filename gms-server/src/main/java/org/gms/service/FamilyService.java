af6ea847e31a95a76c9285396f840226cd8bfa8f
lianhunmo <393811421@qq.com>
2026-04-17 20:04:34 +0800

    规避学院导致启动失败的问题

--- gms-server/src/main/java/org/gms/service/FamilyService.java
+++ gms-server/src/main/java/org/gms/service/FamilyService.java
@@ -78,6 +78,9 @@ public class FamilyService {
         }
         for (World world : Server.getInstance().getWorlds()) {
             for (Family family : world.getFamilies()) {
+                if (family.getLeader() == null) {
+                    continue;
+                }
                 family.getLeader().doFullCount();
             }
         }
