fde6bd19248d5e91b1f0291ace918a1b28d551b8
lianhunmo <393811421@qq.com>
2026-04-21 10:23:38 +0800

    秒伤测试

--- gms-server/scripts-zh-CN/npc/9900001.js
+++ gms-server/scripts-zh-CN/npc/9900001.js
@@ -54,13 +54,13 @@ function action(mode, type, selection) {
         text += "#L11#每日任务#l\t #L14#肝王勋章#l\t #L16#便利商店#l\t #L20#血衣合成#l\r\n";
         text += "#L17#矿石仓库#l\t #L18#卷轴仓库#l\t #L24#怪卡仓库#l\t #L19#怪物卡戒#l\r\n";
         text += "#L21#皇家发型#l\t #L22#皇家整容#l\t #L23#时尚点装#l\t #L26#月卡功能#l\r\n";
-        text += "#L27#金猪腰带#l\t #L28#秒伤测试#l\r\n";
+        text += "#L27#金猪腰带#l\r\n";
         text += "\r\n\r\n#L15#===================#r拍卖行#b===================#k#l\r\n";
         if (cm.getPlayer().isGM()) {
             text += "\r\n\r\n";
             text += "\t\t\t\t#r=====以下内容仅GM可见=====\r\n";
             text += "#L10#一键出售#l\t #L61#超级传送#l\t #L62#超级商店#l\t #L63#整容集合#l \t";
-            text += "#L64#UI查询#l\t #L65#删除道具#l\t #L66#生成道具#l\t  \r\n\r\n";
+            text += "#L64#UI查询#l\t #L65#删除道具#l\t #L66#生成道具#l\t #L70#秒伤测试#l\r\n\r\n";
             text += "#L67#有状态脚本示例#l\t #L68#NextLevel脚本示例#l\t #L69#遗忘技能#l\t #L25#战力排行#l";
         }
         cm.sendSimple(text);
@@ -159,9 +159,6 @@ function doSelect(selection) {
         case 27:
             openNpc("金猪腰带");
             break;
-        case 28:
-            openNpc("秒伤测试");
-            break;
 // GM功能
         case 23:
             openNpc("时尚点装");
@@ -194,6 +191,9 @@ function doSelect(selection) {
             break;
         case 69:
             openNpc("遗忘技能")
+            break;
+        case 70:
+            openNpc("秒伤测试");
             break;
         default:
             cm.sendOk("该功能暂不支持，敬请期待！");
